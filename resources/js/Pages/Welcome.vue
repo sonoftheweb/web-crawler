ecd34fa7d4c67f4ff670c62d76ca2d3bbf52c797
Jacob Ekanem <prometheus6001@gmail.com>
2022-10-23 15:09:29 -0300

    Added initial frontend requirements to trigger crawl.

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,10 +1,18 @@
 <script setup>
-import { Head, Link } from '@inertiajs/inertia-vue3';
+import {Head, Link, useForm} from '@inertiajs/inertia-vue3';
 
 defineProps({
     canLogin: Boolean,
     canRegister: Boolean,
 });
+
+const form = useForm({
+	url: null,
+});
+
+const crawlSite = () => {
+	form.post('/crawl-site');
+}
 </script>
 
 <template>
@@ -26,54 +34,14 @@ defineProps({
                 Laravel Crawler
             </div>
 
-            <div class="mt-8 overflow-hidden">
-                <div class="grid grid-cols-1 md:grid-cols-2 bg-white rounded">
-                    <div class="p-6">
-                        <div class="flex items-center">
-                            <svg
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                viewBox="0 0 24 24"
-                                class="w-8 h-8 text-gray-500"
-                            ><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" /></svg>
-                            <div class="ml-4 text-lg leading-7 font-semibold">
-                                <a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Documentation</a>
-                            </div>
-                        </div>
-
-                        <div class="ml-12">
-                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-                        <div class="flex items-center">
-                            <svg
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                viewBox="0 0 24 24"
-                                class="w-8 h-8 text-gray-500"
-                            ><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" /><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
-                            <div class="ml-4 text-lg leading-7 font-semibold">
-                                <a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">Laracasts</a>
-                            </div>
-                        </div>
-
-                        <div class="ml-12">
-                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+					<form @submit.prevent="crawlSite" class="mt-8 sm:flex sm:items-center md:bg-white md:rounded md:border-gray-300 md:shadow-sm">
+						<div class="w-[500px]">
+							<label for="url" class="sr-only">URL</label>
+							<input type="url" name="url" id="url" class="search-input" v-model="form.url" placeholder="you@example.com" />
+						</div>
+						<button :disabled="form.processing" type="submit" class="crawl-button">Begin Crawl</button>
+					</form>
+					<div class="my-3 text-red-500 text-sm px-3" v-if="form.errors.url">{{ form.errors.url }}</div>
         </div>
     </div>
 </template>
