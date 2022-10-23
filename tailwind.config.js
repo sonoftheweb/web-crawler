851454836eded668e7d6e7d988216fba8be3952e
Jacob Ekanem <prometheus6001@gmail.com>
2022-10-23 13:50:52 -0300

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            blue: colors.blue,
+            cyan: colors.cyan,
+            emerald: colors.emerald,
+            fuchsia: colors.fuchsia,
+            slate: colors.slate,
+            gray: colors.gray,
+            neutral: colors.neutral,
+            stone: colors.stone,
+            green: colors.green,
+            indigo: colors.indigo,
+            lime: colors.lime,
+            orange: colors.orange,
+            pink: colors.pink,
+            purple: colors.purple,
+            red: colors.red,
+            rose: colors.rose,
+            sky: colors.sky,
+            teal: colors.teal,
+            violet: colors.violet,
+            yellow: colors.amber,
+            white: colors.white,
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
